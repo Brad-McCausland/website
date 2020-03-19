55773d44b8b7128c1c4d62d82e396def3d83105c
bmcc0605 <mccausland0605@gmail.com>
2020-03-19 13:06:00 -0700

    Linkifier refined, functions in all known cases.

--- src/utils/Linkify.tsx
+++ src/utils/Linkify.tsx
@@ -23,29 +23,44 @@ function clickableTextLink(link: inLineTextLinkPair)
 }
 
 /*
- * Function clickableTextLink takes a text/link pair and returns an <a> component with it's href set to the supplied url
+ * Function clickableTextLink identifies a linkWord in the given string and splits the string into the text before the link word, a linkified link word, and the remaining text after the link word.
+ * The preceding and trailing substrings are recursed upon to find more key words. The function returns the concatenation of the link with the text on either side.
  */
-function recursiveSplicer(input: any[], ...linkWords: inLineTextLinkPair[])
+function recursiveSplicer(input: string, ...linkWords: inLineTextLinkPair[])
 {
-    var returnText: any[] = [];
+    var returnText: any = input;
 
-    for (var element of input)
+    var substringStart = -1;
+    var currentLinkWord = null;
+
+    // Find the first occurance of any link word and set substringStart, currentLinkWord
+    for (currentLinkWord of linkWords)
     {
-        if (typeof element === "string")
+        substringStart = input.toLowerCase().indexOf(currentLinkWord.text.toLowerCase());
+        if (substringStart > -1)
         {
-            if (linkWords.length > 0)
-            {
-                //TODO: Handle keywords in first, last position. Handle when keywords occur more than once
-                var link = clickableTextLink(linkWords[0]);
-                var substringPosition = element.indexOf(linkWords[0].text);
-                if (substringPosition > -1)
-                {
-                    returnText.push(...[element.slice(0, substringPosition), link, element.slice(substringPosition + linkWords[0].text.length)]);
-                    returnText.push(...recursiveSplicer(returnText, ...linkWords.slice(1)));
-                }
-            }
+            // No need to continue if a match is found
+            break;
         }
     }
+
+    // If linkword found, split input into two substrings on either side of the linkword, recurse on both substrings, then set returnText equal to the combined result
+    if (substringStart > -1 && currentLinkWord !== null)
+    {
+        const substringEnd = substringStart + currentLinkWord.text.length;
+
+        var firstSubstring = input.slice(0, substringStart);
+        var link = clickableTextLink(
+        {
+            // Create new text/url pair with text from input to preserve capitalization
+            text: input.slice(substringStart, substringEnd),
+            url: currentLinkWord.url
+        });
+        var secondSubstring = input.slice(substringEnd);
+
+        returnText = [recursiveSplicer(firstSubstring, ...linkWords), link, recursiveSplicer(secondSubstring, ...linkWords)];
+    }
+
     return returnText;
 }
 
@@ -58,8 +73,8 @@ function recursiveSplicer(input: any[], ...linkWords: inLineTextLinkPair[])
  */
 export function Linkify(text: string, ...linkWords: inLineTextLinkPair[])
 {
-    var content = recursiveSplicer([text], ...linkWords);
-    console.log("huehue: " + content);
+    var content = recursiveSplicer(text, ...linkWords);
+    
     return (
         <BMStyle.ThemeContext.Consumer>
         {theme => (
